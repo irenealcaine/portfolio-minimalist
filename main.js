3686ee31ad52a427e6c233c7ad9f1136ee49804a
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-10-17 16:44:19 +0200

    added portfolio base

--- main.js
+++ main.js
@@ -17,3 +17,24 @@ ScrollReveal().reveal('.name', { delay: 100, origin: "bottom", distance: "60px",
 ScrollReveal().reveal('.photo', { delay: 150, origin: "bottom", distance: "60px", scale: 0.9 });
 ScrollReveal().reveal('.ocupation', { delay: 200, origin: "bottom", distance: "60px", scale: 0.9 });
 ScrollReveal().reveal('.social-media', { delay: 250, origin: "bottom", distance: "60px", scale: 0.9 });
+ScrollReveal().reveal('.buttons', { delay: 300, origin: "bottom", distance: "60px", scale: 0.9 });
+
+//tabs
+const tabs = document.querySelectorAll("[data-target]");
+const tabContents = document.querySelectorAll("[data-content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tc) => {
+      tc.classList.remove("filters__active");
+    });
+    target.classList.add("filters__active");
+
+    tabs.forEach((t) => {
+      t.classList.remove("filter-tab-active");
+    });
+    tab.classList.add("filter-tab-active");
+  });
+});
