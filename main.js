dd015fba5a91b1411a99b3d737c1e2bce1fbafad
Irene Alcaine <irenealcainealvarez@gmail.com>
2022-10-20 23:06:19 +0200

    changed scrollreveal

--- main.js
+++ main.js
@@ -13,13 +13,13 @@ dayNightButton.addEventListener("click", () => {
 });
 
 // Scroll reveal
-ScrollReveal().reveal('.name', { delay: 100, origin: "right", distance: "60px", scale: 0.9 });
-ScrollReveal().reveal('.photo', { delay: 200, origin: "left", distance: "60px", scale: 0.9 });
-ScrollReveal().reveal('.ocupation', { delay: 300, origin: "right", distance: "60px", scale: 0.9 });
-ScrollReveal().reveal('.social-media', { delay: 400, origin: "left", distance: "60px", scale: 0.9 });
-ScrollReveal().reveal('.buttons', { delay: 500, origin: "right", distance: "60px", scale: 0.9 });
-ScrollReveal().reveal('main', { delay: 700, origin: "left", distance: "60px", scale: 1 });
-ScrollReveal().reveal('footer', { delay: 300, origin: "right", distance: "60px", scale: 0.9 });
+ScrollReveal().reveal('.name', { delay: 100, origin: "right", distance: "160px", scale: 0.9 });
+ScrollReveal().reveal('.photo', { delay: 200, origin: "left", distance: "160px", scale: 0.9 });
+ScrollReveal().reveal('.ocupation', { delay: 300, origin: "right", distance: "160px", scale: 0.9 });
+ScrollReveal().reveal('.social-media', { delay: 400, origin: "left", distance: "160px", scale: 0.9 });
+ScrollReveal().reveal('.buttons', { delay: 500, origin: "right", distance: "160px", scale: 0.9 });
+ScrollReveal().reveal('main', { delay: 700, origin: "left", distance: "160px", scale: 1 });
+ScrollReveal().reveal('footer', { delay: 300, origin: "right", distance: "160px", scale: 0.9 });
 
 //tabs
 const tabs = document.querySelectorAll("[data-target]");
